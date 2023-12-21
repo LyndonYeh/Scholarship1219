d0c1d38876c00df3079c8d15700514ad206841eb
Dave <dave.wenyu@gmail.com>
2023-12-21 10:51:13 +0800

    路徑修正

--- src/main/java/scholarship/controller/ScholarshipMySQLController.java
+++ src/main/java/scholarship/controller/ScholarshipMySQLController.java
@@ -35,58 +35,67 @@ public class ScholarshipMySQLController {
 
 
 
-//	// private Logger logger = LoggerFactory.getLogger(getClass());
-//	/*
-//	 * 測試用hello為測試用，測試能否從網址連到controller
-//	 */
-//
-//	@GetMapping("/hello")
-//	@ResponseBody
-//	public String hello(HttpServletRequest req, HttpServletResponse resp) {
-//		// logger.info("hello, spirng 來了!!!");
-//		return "Hello, Spring12345 !!";
-//	}
-//
-//	/*
-//	 * 透過網址連到登入頁
-//	 */
-//	@GetMapping(value = {"/login", "/", "/login/"})
-//	public String loginPage() {
-//		return "/login";
-//	}
-//	
-//	
-//	@PostMapping("/login")
-//	public String login(@RequestParam("username") String username, 
-//						 @RequestParam("password") String password, 
-//						HttpSession session, Model model) {
-//		// 根據 username 查找 user 物件
-//		Optional<User> userOpt = userDao.findUserByUsername(username);
-//		if(userOpt.isPresent()) {
-//			User user = userOpt.get();
-//			// 比對 password
-//			if(user.getPassword().equals(password)) {
-//				session.setAttribute("user", user); // 將 user 物件放入到 session 變數中
-//				return "redirect:/mvc/backendtest"; // OK, 導向後台首頁
-//			} else {
-//				session.invalidate(); // session 過期失效
-//				model.addAttribute("loginMessage", "密碼錯誤");
-//				return "mvc/login";
-//			}
-//		} else {
-//			session.invalidate(); // session 過期失效
-//			model.addAttribute("loginMessage", "無此使用者");
-//			return "mvc/login";
-//		}
-//	}
-//	
-//	// 登入後重新導向的 後台測試頁, 若 後台 controller 串接好此路徑可刪除
-//	@GetMapping("/backendtest")
-//	@ResponseBody
-//	public String backendtest(HttpServletRequest req, HttpServletResponse resp) {
-//		return "backendTest 後台測試頁 登入成功 !";
-//
-//	}
+	// private Logger logger = LoggerFactory.getLogger(getClass());
+	/*
+	 * 測試用hello為測試用，測試能否從網址連到controller
+	 */
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello(HttpServletRequest req, HttpServletResponse resp) {
+		// logger.info("hello, spirng 來了!!!");
+		return "Hello, Spring12345 !!";
+	}
+
+	/*
+	 * 透過網址連到登入頁
+	 */
+	
+	@GetMapping(value = {"/login", "/", "/login/"})
+	public String loginPage() {
+		return "/login";
+	}
+	
+	
+	@PostMapping("/login")
+	public String login(@RequestParam("username") String username, 
+						 @RequestParam("password") String password, 
+						HttpSession session, Model model) {
+		// 根據 username 查找 user 物件
+		Optional<User> userOpt = userDao.findUserByUsername(username);
+		if(userOpt.isPresent()) {
+			User user = userOpt.get();
+			// 比對 password
+			if(user.getPassword().equals(password)) {
+				session.setAttribute("user", user); // 將 user 物件放入到 session 變數中
+				return "redirect:/mvc/scholarship/backendtest"; // OK, 導向後台首頁
+			} else {
+				session.invalidate(); // session 過期失效
+				model.addAttribute("loginMessage", "密碼錯誤");
+				return "login";
+			}
+		} else {
+			session.invalidate(); // session 過期失效
+			model.addAttribute("loginMessage", "無此使用者");
+			return "login";
+		}
+	}
+	
+	// 登入後重新導向的 後台測試頁, 若 後台 controller 串接好此路徑可刪除
+	@GetMapping("/backendtest")
+	@ResponseBody
+	public String backendtest(HttpServletRequest req, HttpServletResponse resp) {
+		return "backendTest 後台測試頁 登入成功 !";
+
+	}
+	
+	@GetMapping("/backend")
+	public String index(@ModelAttribute Scholarship scholarship, Model model) {
+		addBasicModel(model);
+		model.addAttribute("submitBtnName", "新增");
+		model.addAttribute("_method", "POST");
+		return "backend/backendmain";
+	}
 	
 
 	/*
@@ -110,13 +119,8 @@ public class ScholarshipMySQLController {
 		model.addAttribute("users", users); // 取得目前最新 users 資料
 	}
 
-	@GetMapping("/")
-	public String index(@ModelAttribute Scholarship scholarship, Model model) {
-		addBasicModel(model);
-		model.addAttribute("submitBtnName", "新增");
-		model.addAttribute("_method", "POST");
-		return "backend/backendmain";
-	}
+
+	
 
 	@PostMapping("/main") // 新增 User
 	public String addUser(@Valid Scholarship scholarship, BindingResult result, Model model) { // @Valid 驗證, BindingResult 驗證結果
